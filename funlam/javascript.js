87569c82bce43392fac10f07198fc847b0a9a0c8
nicolas <mejianicolasg2016mail.com>
2023-02-23 11:16:56 -0500

    commit 2

--- funlam/javascript.js
+++
@@ -1,5 +0,0 @@
-window.alert("deseas acceder a la pagina");
-x = 5;
-y = 6;
-let z = x + y;
-document.write("la suma de: ",x," mas el numero: ",y," es igual a:",z)
---
+++ nicolasm2/javascript.js
@@ -0,0 +1,21 @@
+window.alert("deseas acceder a la pagina");
+x = 5;
+y = 6;
+let z = x + y;
+document.write("la suma de: ",x," mas el numero: ",y," es igual a:",z)
+
+/*funciones en javascript*/
+/*se de calra la funcion con function */
+/* se le pone le nombre y entre parentesis los argumentos */
+function imprimir(){
+    document.write("funcion imprimir")
+}
+imprimir();
+
+/* variables  */
+var number = 15
+
+function square(number) {
+    return number * number;
+  }
+square();
